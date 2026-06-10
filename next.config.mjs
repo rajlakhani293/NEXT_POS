776cbc83c3b8e87b0b53f641c57926f36f1055f1
rajlakhani293 <lakhaniraj293@gmail.com>
2026-06-10 21:00:35 +0530

    feat: enable optimizePackageImports for lucide-react and react-icons in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        optimizePackageImports: ["lucide-react", "react-icons"],
+    },
+}
 
 export default nextConfig
